71140ae9927d0c098b05096adf5db552e7d8e7f8
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-12 17:42:17 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const message = document.createElement("div");
+    message.classList.add("notification", type);
+    message.style.top = `${posTop}px`;
+    message.style.right = `${posRight}px`;
+    const messageTitle = document.createElement("h2");
+    const messageDescription = document.createElement("p");
+    messageTitle.className = "title";
+    messageTitle.textContent = title;
+    messageDescription.textContent = description;
+    message.append(messageTitle, messageDescription);
+    document.body.append(message);
+    setTimeout(()=>message.style.visibility = "hidden", 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
